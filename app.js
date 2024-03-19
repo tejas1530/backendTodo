94034d6e6d66f24231b056208067aeedd53f7506
tejas1530 <bhatttejas30@gmail.com>
2024-03-20 01:56:52 +0530

    todos ref

--- app.js
+++ app.js
@@ -7,7 +7,14 @@ dotenv.config()
 
 const app = express() 
 
-app.use(cors())
+app.use(cors(
+    {
+        origin: ["http://localhost:4000","https://backend-todo-virid.vercel.app"],
+        methods: ["GET", "POST", "PUT", "DELETE"],
+        allowedHeaders: ["Content-Type", "Authorization"],
+        credentials: true
+    }
+))
 
 app.get("/", (req, res) => {
     res.send("Hello World");
@@ -18,7 +25,9 @@ app.use(express.urlencoded({ extended: true }));
 
 // routes 
 
-app.use("/api/v1/user",router.authRouter)
-
+app.use("/api/v1/auth",router.authRouter)
+app.use("/api/v1/user", router.userRouter)
+app.use("/api/v1/category", router.categoryRouter)
+app.use("/api/v1/todo", router.TodoRouter)
 
 export default app
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,13 @@
 import auth from "./auth.routes.js";
+import category from "./category.routes.js";
+import todo from "./todo.routes.js";
+import user from "./users.routes.js";
 
 const router ={
-    authRouter:auth
+    authRouter:auth,
+    userRouter:user,
+    categoryRouter:category,
+    TodoRouter:todo
 }
 
 export default router;
